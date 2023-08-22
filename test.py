8f29f373dd5a4aa2d372bc59dd2309b8c182071c
Max Bernstein <max@bernsteinbear.com>
2023-08-22 13:39:14 -0400

    Add compiler demo

---
+++ test.py
@@ -0,0 +1,42 @@
+import random
+from micrograd import nn
+from micrograd.engine import Value
+
+random.seed(1337)
+n = nn.MLP(2, [16, 16, 1])
+x = [Value(i) for i in range(16)]
+print("""
+template <typename T = double, int dim = 1>
+class Vector {
+ public:
+  Vector<T, dim>(T other[dim]) {
+    for (int i = 0; i < dim; i++) {
+      arr[i] = other[i];
+    }
+  }
+
+  Vector<T, dim> dot(Vector<T, dim> other) {
+    T result[dim];
+    for (int i = 0; i < dim; i++) {
+      result[i] = arr[i] * other.arr[i];
+    }
+    return result;
+  }
+
+  T sum() {
+    T result = 0;
+    for (int i = 0; i < dim; i++) {
+      result += arr[i];
+    }
+    return result;
+  }
+
+  T& at(int idx) { return arr[idx]; }
+
+ private:
+  T arr[dim];
+};
+""")
+print("\n".join(n.compile(x)))
+# y = n(x)
+# print(y, n)
