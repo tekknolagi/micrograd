08be7ebafef16390293d8e57103d85301b06a8b4
Max Bernstein <max@bernsteinbear.com>
2023-09-01 16:23:23 -0400

    Add timer to see how long each phase takes

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ import random
 import shutil
 import struct
 import tempfile
+import time
 from distutils import sysconfig
 from micrograd import nn as nn_interp
 from micrograd.engine import Value
@@ -66,11 +67,21 @@ class images:
         return self.num_images - self.idx
 
 
-print("Opening images...")
-db = images("train-images-idx3-ubyte", "train-labels-idx1-ubyte")
-print("Building model...")
+def timer(lam, msg=""):
+    print(msg, end=" ")
+    before = time.perf_counter()
+    result = lam()
+    after = time.perf_counter()
+    delta = after - before
+    print(f"({delta:.2f} s)")
+    return result
+
+
+db = timer(lambda: images("train-images-idx3-ubyte", "train-labels-idx1-ubyte"),
+           "Opening images...")
 NUM_DIGITS = 10
-model = nn_interp.MLP(DIM, [50, NUM_DIGITS])
+model = timer(lambda: nn_interp.MLP(DIM, [512, NUM_DIGITS]),
+              "Building model...")
 # NOTE: It's important that input are all in sequence right next to one another
 # so we can set the input in training
 inp = [Value(0, (), "input") for _ in range(DIM)]
@@ -86,170 +97,170 @@ loss = sum((exp - act) ** 2 for exp, act in zip(expected_onehot, out))
 topo = loss.topo()
 # TODO(max): Figure out why there are (significant numbers of) duplicated
 # Values
-topo = list(dict.fromkeys(topo))  # dedup and maintain order
+topo = timer(lambda: list(dict.fromkeys(topo)), "Deduping...")  # dedup and maintain order
 num_nodes = len(topo)
 assert num_nodes == len(set(topo)), f"{len(topo)-len(set(topo))} duplicates"
 assert num_nodes == micrograd.engine.counter, f"{len(topo)} vs {micrograd.engine.counter}"
 
-print("Writing C code...")
-with tempfile.TemporaryDirectory() as dir_path:
-    source_dir = f"{dir_path}/src"
-    build_dir = f"{dir_path}/build"
-    os.makedirs(source_dir)
-    os.makedirs(build_dir)
-    file_path = f"{source_dir}/nn.c"
-    with open(file_path, "w+") as f:
-        print(
-            f"""\
+def write_code():
+    with tempfile.TemporaryDirectory() as dir_path:
+        source_dir = f"{dir_path}/src"
+        build_dir = f"{dir_path}/build"
+        os.makedirs(source_dir)
+        os.makedirs(build_dir)
+        file_path = f"{source_dir}/nn.c"
+        with open(file_path, "w+") as f:
+            print(
+                f"""\
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
 #include <Python.h>
-double data[{num_nodes}];
-double grad[{num_nodes}];
-double relu(double x) {{ if (x < 0) {{ return 0; }} else {{ return x; }} }}
-void init() {{
-        """,
-            file=f,
-        )
-        for o in model.parameters():
-            print(f"data[{o._id}] = {o.data}L;", file=f)
-        print("}", file=f)
-        print(
-            f"""\
-void set_input(PyObject* input_data) {{
-    const char* buf = PyBytes_AsString(input_data);
-    if (buf == NULL) {{
-        abort();
+    double data[{num_nodes}];
+    double grad[{num_nodes}];
+    double relu(double x) {{ if (x < 0) {{ return 0; }} else {{ return x; }} }}
+    void init() {{
+            """,
+                file=f,
+            )
+            for o in model.parameters():
+                print(f"data[{o._id}] = {o.data}L;", file=f)
+            print("}", file=f)
+            print(
+                f"""\
+    void set_input(PyObject* input_data) {{
+        const char* buf = PyBytes_AsString(input_data);
+        if (buf == NULL) {{
+            abort();
+        }}
+        for (int i = 0; i < {DIM}; i++) {{
+            data[{inp[0]._id}+i] = buf[i];
+        }}
     }}
-    for (int i = 0; i < {DIM}; i++) {{
-        data[{inp[0]._id}+i] = buf[i];
+            """,
+                file=f,
+            )
+            print("void forward() {", file=f)
+            for o in topo:
+                line = o.compile()
+                if line:
+                    print(line, file=f)
+            print("}", file=f)
+            print("void backward() {", file=f)
+            print(f"grad[{loss._id}] = 1;", file=f)
+            for o in reversed(topo):
+                for line in o.backward_compile():
+                    print(line, file=f)
+            print("}", file=f)
+            print("void update(int step) {", file=f)
+            # TODO(max): It's not always 100; is this hard-coded for number of
+            # training rounds in Karpathy's code?
+            print("double learning_rate = 1.0L - (0.9L * (double)step) / 100.0L;", file=f)
+            for o in model.parameters():
+                print(f"data[{o._id}] -= learning_rate * grad[{o._id}];", file=f)
+            print("}", file=f)
+            print(
+                f"""\
+    PyObject* forward_wrapper(PyObject *module, PyObject *const *args, Py_ssize_t nargs) {{
+          if (nargs != 2) {{
+                PyErr_Format(PyExc_TypeError, "expected 2 args: label, pixels");
+                return NULL;
+          }}
+          PyObject* label_obj = args[0];
+          PyObject* pixels_obj = args[1];
+          if (!PyLong_CheckExact(label_obj)) {{
+                PyErr_Format(PyExc_TypeError, "expected int");
+                return NULL;
+          }}
+          if (!PyBytes_CheckExact(pixels_obj)) {{
+                PyErr_Format(PyExc_TypeError, "expected bytes");
+                return NULL;
+          }}
+          if (PyBytes_Size(pixels_obj) != {DIM}) {{
+                PyErr_Format(PyExc_TypeError, "expected bytes of size {DIM}");
+                return NULL;
+          }}
+          int label = PyLong_AsLong(label_obj);
+          if (label < 0 && PyErr_Occurred()) {{
+                return NULL;
+          }}
+          // Set label
+          memset(&data[{expected_onehot[0]._id}], 0, {NUM_DIGITS}*sizeof data[0]);
+          data[{expected_onehot[0]._id}+label] = 1.0L;
+          set_input(pixels_obj);
+          forward();
+          // TODO(max): Make this able to return multiple outputs?
+          double loss = data[{loss._id}];
+          return PyFloat_FromDouble(loss);
     }}
-}}
-        """,
-            file=f,
-        )
-        print("void forward() {", file=f)
-        for o in topo:
-            line = o.compile()
-            if line:
-                print(line, file=f)
-        print("}", file=f)
-        print("void backward() {", file=f)
-        print(f"grad[{loss._id}] = 1;", file=f)
-        for o in reversed(topo):
-            for line in o.backward_compile():
-                print(line, file=f)
-        print("}", file=f)
-        print("void update(int step) {", file=f)
-        # TODO(max): It's not always 100; is this hard-coded for number of
-        # training rounds in Karpathy's code?
-        print("double learning_rate = 1.0L - (0.9L * (double)step) / 100.0L;", file=f)
-        for o in model.parameters():
-            print(f"data[{o._id}] -= learning_rate * grad[{o._id}];", file=f)
-        print("}", file=f)
-        print(
-            f"""\
-PyObject* forward_wrapper(PyObject *module, PyObject *const *args, Py_ssize_t nargs) {{
-      if (nargs != 2) {{
-            PyErr_Format(PyExc_TypeError, "expected 2 args: label, pixels");
-            return NULL;
-      }}
-      PyObject* label_obj = args[0];
-      PyObject* pixels_obj = args[1];
-      if (!PyLong_CheckExact(label_obj)) {{
-            PyErr_Format(PyExc_TypeError, "expected int");
-            return NULL;
-      }}
-      if (!PyBytes_CheckExact(pixels_obj)) {{
-            PyErr_Format(PyExc_TypeError, "expected bytes");
-            return NULL;
-      }}
-      if (PyBytes_Size(pixels_obj) != {DIM}) {{
-            PyErr_Format(PyExc_TypeError, "expected bytes of size {DIM}");
-            return NULL;
-      }}
-      int label = PyLong_AsLong(label_obj);
-      if (label < 0 && PyErr_Occurred()) {{
-            return NULL;
-      }}
-      // Set label
-      memset(&data[{expected_onehot[0]._id}], 0, {NUM_DIGITS}*sizeof data[0]);
-      data[{expected_onehot[0]._id}+label] = 1.0L;
-      set_input(pixels_obj);
-      forward();
-      // TODO(max): Make this able to return multiple outputs?
-      double loss = data[{loss._id}];
-      return PyFloat_FromDouble(loss);
-}}
 
-PyObject* zero_grad_wrapper(PyObject* module) {{
-      // Don't just zero the parameters; Karpathy can get away with that
-      // because he rebuilds the entire graph every time, but we don't.
-      memset(grad, 0, sizeof grad);
-      Py_RETURN_NONE;
-}}
-
-PyObject* backward_wrapper(PyObject* module) {{
-      backward();
-      Py_RETURN_NONE;
-}}
-
-PyObject* update_wrapper(PyObject* module, PyObject* step_obj) {{
-      int step = PyLong_AsLong(step_obj);
-      if (step < 0 && PyErr_Occurred()) {{
-            return NULL;
-      }}
-      update(step);
-      Py_RETURN_NONE;
-}}
-
-static PyMethodDef nn_methods[] = {{
-      {{ "forward", (PyCFunction)forward_wrapper, METH_FASTCALL, "doc" }},
-      {{ "zero_grad", (PyCFunction)zero_grad_wrapper, METH_NOARGS, "doc" }},
-      {{ "backward", (PyCFunction)backward_wrapper, METH_NOARGS, "doc" }},
-      {{ "update", update_wrapper, METH_O, "doc" }},
-      {{ NULL, NULL }},
-}};
-
-// clang-format off
-static struct PyModuleDef nnmodule = {{
-    PyModuleDef_HEAD_INIT,
-    "nn",
-    "doc",
-    -1,
-    nn_methods,
-    NULL,
-    NULL,
-    NULL,
-    NULL
-}};
-// clang-format on
-
-PyObject* PyInit_nn() {{
-    PyObject* m = PyState_FindModule(&nnmodule);
-    if (m != NULL) {{
-        return m;
+    PyObject* zero_grad_wrapper(PyObject* module) {{
+          // Don't just zero the parameters; Karpathy can get away with that
+          // because he rebuilds the entire graph every time, but we don't.
+          memset(grad, 0, sizeof grad);
+          Py_RETURN_NONE;
     }}
-    init();
-    return PyModule_Create(&nnmodule);
-}}""",
-            file=f,
-        )
 
-    print("Compiling extension...")
-    include_dir = sysconfig.get_python_inc()
-    lib_file = f"{build_dir}/nn.so"
-    shutil.copyfile(f.name, "nn.c")
-    # TODO(max): Bring back Extension stuff and customize compiler using
-    # https://shwina.github.io/custom-compiler-linker-extensions/
-    os.system(f"ccache tcc -shared -fPIC -I{include_dir} {file_path} -o {lib_file}")
-    shutil.copyfile(lib_file, "nn.so")
+    PyObject* backward_wrapper(PyObject* module) {{
+          backward();
+          Py_RETURN_NONE;
+    }}
 
-    print("Loading extension...")
-    spec = importlib.machinery.ModuleSpec("nn", None, origin=lib_file)
-    nn = _imp.create_dynamic(spec)
+    PyObject* update_wrapper(PyObject* module, PyObject* step_obj) {{
+          int step = PyLong_AsLong(step_obj);
+          if (step < 0 && PyErr_Occurred()) {{
+                return NULL;
+          }}
+          update(step);
+          Py_RETURN_NONE;
+    }}
 
+    static PyMethodDef nn_methods[] = {{
+          {{ "forward", (PyCFunction)forward_wrapper, METH_FASTCALL, "doc" }},
+          {{ "zero_grad", (PyCFunction)zero_grad_wrapper, METH_NOARGS, "doc" }},
+          {{ "backward", (PyCFunction)backward_wrapper, METH_NOARGS, "doc" }},
+          {{ "update", update_wrapper, METH_O, "doc" }},
+          {{ NULL, NULL }},
+    }};
+
+    // clang-format off
+    static struct PyModuleDef nnmodule = {{
+        PyModuleDef_HEAD_INIT,
+        "nn",
+        "doc",
+        -1,
+        nn_methods,
+        NULL,
+        NULL,
+        NULL,
+        NULL
+    }};
+    // clang-format on
+
+    PyObject* PyInit_nn() {{
+        PyObject* m = PyState_FindModule(&nnmodule);
+        if (m != NULL) {{
+            return m;
+        }}
+        init();
+        return PyModule_Create(&nnmodule);
+    }}""",
+                file=f,
+            )
+        local_name = "nn.c"
+        shutil.copyfile(f.name, local_name)
+    return local_name
+
+include_dir = sysconfig.get_python_inc()
+source_file = timer(lambda: write_code(), "Writing C code...")
+# TODO(max): Bring back Extension stuff and customize compiler using
+# https://shwina.github.io/custom-compiler-linker-extensions/
+lib_file = "nn.so"
+timer(lambda: os.system(f"tcc -shared -fPIC -I{include_dir} nn.c -o {lib_file}"),
+      "Compiling extension...")
+
+spec = importlib.machinery.ModuleSpec("nn", None, origin=lib_file)
+nn = timer(lambda: _imp.create_dynamic(spec), "Loading extension...")
 
 losses = collections.deque((), maxlen=16)
 
