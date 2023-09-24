5a7f16804ed29ec6f0b5c8e9d214f4240cd85255
Max Bernstein <max@bernsteinbear.com>
2023-09-24 13:32:58 -0400

    Add _forward and topo functions

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -2,13 +2,14 @@ import math
 
 class Value:
     """ stores a single scalar value and its gradient """
-    __slots__ = ("data", "grad", "_backward", "_prev", "_op")
+    __slots__ = ("data", "grad", "_backward", "_forward", "_prev", "_op")
 
     def __init__(self, data, _children=(), _op=''):
         self.data = data
         self.grad = 0
         # internal variables used for autograd graph construction
         self._backward = lambda: None
+        self._forward = lambda: None
         self._prev = _children
         self._op = _op # the op that produced this node, for graphviz / debugging / etc
 
@@ -21,6 +22,10 @@ class Value:
             other.grad += out.grad
         out._backward = _backward
 
+        def _forward():
+            out.data = self.data + other.data
+        out._forward = _forward
+
         return out
 
     def __mul__(self, other):
@@ -32,6 +37,10 @@ class Value:
             other.grad += self.data * out.grad
         out._backward = _backward
 
+        def _forward():
+            out.data = self.data * other.data
+        out._forward = _forward
+
         return out
 
     def __pow__(self, other):
@@ -42,6 +51,10 @@ class Value:
             self.grad += (other * self.data**(other-1)) * out.grad
         out._backward = _backward
 
+        def _forward():
+            out.data = self.data ** other
+        out._forward = _forward
+
         return out
 
     def relu(self):
@@ -51,6 +64,10 @@ class Value:
             self.grad += (out.data > 0) * out.grad
         out._backward = _backward
 
+        def _forward():
+            out.data = 0 if self.data < 0 else self.data
+        out._forward = _forward
+
         return out
 
     def exp(self):
@@ -58,6 +75,9 @@ class Value:
         def _backward():
             self.grad += math.exp(self.data) * out.grad
         out._backward = _backward
+        def _forward():
+            out.data = math.exp(self.data)
+        out._forward = _forward
         return out
 
     def log(self):
@@ -65,7 +85,24 @@ class Value:
         def _backward():
             self.grad += 1/self.data * out.grad
         out._backward = _backward
+        def _forward():
+            out.data = math.log(self.data)
+        out._forward = _forward
         return out
+
+    def topo(self):
+
+        # topological order all of the children in the graph
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        return topo
 
     def backward(self):
 
@@ -118,4 +155,26 @@ class Max(Value):
                 left.grad += self.grad
             else:
                 right.grad += self.grad
+        self._backward = _backward
+        def _forward():
+            self.data = max(left.data, right.data)
+        self._forward = _forward
+
+
+def dot(l, r):
+    return sum(li.data*ri.data for li,ri in zip(l,r))
+
+
+class Dot(Value):
+    def __init__(self, left_arr, right_arr):
+        assert len(left_arr) == len(right_arr)
+        assert left_arr
+        super().__init__(dot(left_arr, right_arr), tuple(set(left_arr+right_arr)), 'dot')
+        def _forward():
+            self.data = dot(left_arr, right_arr)
+        self._forward = _forward
+        def _backward():
+            for li, ri in zip(left_arr, right_arr):
+                li.grad += ri.data*self.grad
+                ri.grad += li.data*self.grad
         self._backward = _backward
