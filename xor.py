e8207bb2baafc616adc1bafe89413b5b4d71e7ad
Max Bernstein <max@bernsteinbear.com>
2023-09-27 11:39:40 -0400

    wip

--- xor.py
+++ xor.py
@@ -1,22 +1,24 @@
 import random
 from micrograd.nn import MLP
-random.seed(1)
-model = MLP(2, [4, 1])
+random.seed(4)
+model = MLP(2, [5, 1])
 batch = [([0, 0], 0), ([0, 1], 1), ([1, 0], 1), ([1, 1], 0)]
 batch_size = len(batch)
-for epoch in range(100):
+for epoch in range(1000):
     model.zero_grad()
     epoch_loss = 0.
-    for xs, expected in batch:
-        output = model(xs)
-        loss = (output-expected)**2
-        epoch_loss += loss.data
-        loss.backward()
+    outputs = [model(xs) for xs, _ in batch]
+    expected = [exp for _, exp in batch]
+    losses = [(exp-act)**2 for exp, act in zip(expected, outputs)]
+    loss = sum(losses) * (1.0 / len(losses))
+    loss.backward()
     for p in model.parameters():
         p.data -= 0.1 * p.grad / batch_size
-    print(f"...epoch {epoch:4d} loss {epoch_loss/batch_size:.4f}")
+    if epoch % 100 == 0:
+        print(f"...epoch {epoch:4d} loss {loss.data/batch_size:.4f}")
 
 
+print("params", [p.data for p in model.parameters()])
 for im in batch:
     result = model(im[0])
     assert abs(result.data-im[1]) < 0.1, f"{result.data:.4f} is not near {im[1]}"
