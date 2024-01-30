65342def8822cc161b487659af4998009e930610
Max Bernstein <max@bernsteinbear.com>
2024-01-30 01:20:22 -0500

    Optimize topo sort

--- micrograd/engine_test.py
+++ micrograd/engine_test.py
@@ -1,12 +1,24 @@
 import unittest
 from micrograd.engine import Value
 
-def optimize(v):
+def optimize_one(v):
     if v._op == '+':
         left, right = v.args()
         if left._op == '' and right._op == '':
             v.make_equal_to(Value(left.data+right.data))
             return
+    if v._op == '*':
+        left, right = v.args()
+        if left._op == '' and right._op == '':
+            v.make_equal_to(Value(left.data+right.data))
+            return
+
+
+def optimize(v):
+    topo = v.topo()
+    for op in topo:
+        optimize_one(op)
+
 
 class Tests(unittest.TestCase):
     def test_const(self):
@@ -38,8 +50,18 @@ class Tests(unittest.TestCase):
     def test_const_fold_plus(self):
         x = Value(1)+2
         self.assertEqual(x.find()._op, '+')
+        optimize_one(x)
+        self.assertEqual(x.find()._op, '')
+        self.assertEqual(x.find().data, 3)
+
+    def test_const_fold_multiple_plus(self):
+        x = (Value(1)+2)+(Value(3)+4)
+        self.assertEqual(x.find()._op, '+')
+        optimize_one(x)
+        self.assertEqual(x.find()._op, '+')
         optimize(x)
         self.assertEqual(x.find()._op, '')
+        self.assertEqual(x.find().data, 10)
 
 
 if __name__ == "__main__":
